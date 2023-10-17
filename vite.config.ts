7fe518f7cdfbb45269d6fcf9d342e4b01d71f5ac
ladaland <ladamoneone@gmail.com>
2023-10-18 03:51:33 +0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,22 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd(), "");
   console.log(env.SERVICE_ID, env.TEMPLATE_ID, env.EMAIL_PUBLIC_KEY);
-  return {
+  const config = {
     define: {
       "process.env.EMAIL_PUBLIC_KEY": JSON.stringify(env.EMAIL_PUBLIC_KEY),
       "process.env.SERVICE_ID": JSON.stringify(env.SERVICE_ID),
       "process.env.TEMPLATE_ID": JSON.stringify(env.TEMPLATE_ID),
     },
     plugins: [react()],
+    base: "/",
   };
+
+  if (command !== "serve") {
+    config.base = "/animation-resume/";
+  }
+
+  return config;
 });
